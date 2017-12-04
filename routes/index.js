936501a31b343a0b2c3a0e6e9215a15a1db57e8a
kyungee <kyungee.kim@gmail.com>
2017-12-04 22:42:59 +0900

    add : 데이터베이스 연결 및 회원가입 서버코드 추가

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,53 @@
+
 var express = require('express');
+var mysql = require('mysql');
+
+var mysql_dbc = require('../config/db_info.js');
+var connection = mysql.createConnection(mysql_dbc);
+
 var router = express.Router();
+
+connection.connect(function(err) {
+    if (err) {
+        console.error('mysql connection error');
+        console.error(err);
+        throw err;
+    }
+});
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express'});
 });
 
+// GET : REGISTER PAGE
 router.get('/register', function(req, res, next) {
-  res.render('register', { title: 'Express'});
+  res.render('register');
+});
+
+// POST : REGISTER PAGE
+router.post('/register', function(req, res, next) {
+  console.log('aaa');
+  var user = {
+    'user_id': req.body.userid,
+    'pw': req.body.pw,
+    'name': req.body.username,
+    'type': req.body.class,
+    'phone_num': req.body.phonenum,
+    'email': req.body.email,
+    'address': req.body.address
+  };
+
+  var stmt = 'INSERT into USER set ?';
+  connection.query(stmt, user, function (err, result) {
+    if (err){
+      console.error(err);
+      throw err;
+    } else{
+      res.render('login');
+    }
+  })
 });
 
 router.get('/login', function(req, res, next) {
