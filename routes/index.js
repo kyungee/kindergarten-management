6e7c5fdcf582bf01f50e56df8d03769188b9bf61
kyungee <kyungee.kim@gmail.com>
2017-12-12 15:30:46 +0900

    add: 회원가입 및 로그인 추가

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 
 var express = require('express');
 var mysql = require('mysql');
+var session = require('express-session');
 
 var mysql_dbc = require('../config/db_info.js');
 var connection = mysql.createConnection(mysql_dbc);
@@ -18,7 +19,14 @@ connection.connect(function(err) {
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express'});
+  var sendData = {}
+
+  if(req.session)
+    sendData.mem_name = req.session.name;
+  else
+    sendData.mem_name = null;
+
+  res.render('index', sendData);
 });
 
 // GET : REGISTER PAGE
@@ -28,7 +36,6 @@ router.get('/register', function(req, res, next) {
 
 // POST : REGISTER PAGE
 router.post('/register', function(req, res, next) {
-  console.log('aaa');
   var user = {
     'user_id': req.body.userid,
     'pw': req.body.pw,
@@ -50,8 +57,37 @@ router.post('/register', function(req, res, next) {
   })
 });
 
+// GET : LOGIN PAGE
 router.get('/login', function(req, res, next) {
-  res.render('login', { title: 'Express'});
+  var sendData = {}
+
+  if(req.session)
+    sendData.mem_name = req.session.name;
+  else
+    sendData.mem_name = null;
+
+  res.render('login', sendData);
+});
+
+// POST : LOGIN PAGE
+router.post('/login', function(req, res, next) {
+  var user = {
+    'userid': req.body.userid,
+    'pw': req.body.pw
+  };
+
+  var stmt = 'SELECT * FROM USER WHERE user_id="'+user.userid+'" and pw="'+user.pw+'"';
+  connection.query(stmt, function (err, rows) {
+    console.log("rows : " + JSON.stringify(rows));
+    if (err){
+      console.error(err);
+      throw err;
+    } else{
+      req.session.user_id = rows[0]['user_id'];
+      req.session.name = rows[0]['name'];
+      res.redirect('/');
+    }
+  })
 });
 
 module.exports = router;
