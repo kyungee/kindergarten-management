786aff1c512ec3a197ca3a7b95096a179383757a
kyungee <kyungee.kim@gmail.com>
2017-12-12 19:45:50 +0900

    add: 유치원생 관리 프론트 페이지 추가

--- routes/kids.js
+++ routes/kids.js
@@ -24,4 +24,26 @@ router.get('/detail', function(req, res, next) {
   res.render('kids_detail', sendData);
 });
 
+router.get('/add', function(req, res, next) {
+  var sendData = {}
+
+  if(req.session)
+    sendData.mem_name = req.session.name;
+  else
+    sendData.mem_name = null;
+
+  res.render('kids_add', sendData);
+});
+
+router.get('/edit', function(req, res, next) {
+  var sendData = {}
+
+  if(req.session)
+    sendData.mem_name = req.session.name;
+  else
+    sendData.mem_name = null;
+
+  res.render('kids_edit', sendData);
+});
+
 module.exports = router;
